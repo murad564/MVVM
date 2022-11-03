ca2f0a74bdc31a7549ffd0d83c5121be6d62c8ce
Mehe_od76@itstep.edu.az <Mehe_od76@itstep.edu.az>
2022-11-03 13:41:31 +0400

    .

---
+++ MVVM/UserControls/CustomUC.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MVVM.UserControls
+{
+    public partial class CustomUC : UserControl
+    {
+        public CustomUC()=>InitializeComponent();
+    }
+}
+
+
